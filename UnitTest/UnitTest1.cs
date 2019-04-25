e8cce08aee68f554cf42c21f25f3b277f5a499cd
Clifford Beta <betaclifford@gmail.com>
2019-04-25 21:47:17 +0300

    Add more test cases
    Signed-off-by: Clifford Beta <betaclifford@gmail.com>

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -17,6 +17,7 @@ namespace UnitTest
             var lines = File.ReadAllLines("../../../test.txt");
             Employees employees = new Employees(lines);
             Assert.Equal(3300,employees.SalaryBudget("Employee1"));
+            Assert.Equal(1000,employees.SalaryBudget("Employee2"));
  
         }
         
@@ -26,6 +27,31 @@ namespace UnitTest
             var lines = File.ReadAllLines("../../../test1.txt");
             Employees employees = new Employees(lines);
             Assert.Equal(3300,employees.SalaryBudget("Employee1"));
+            Assert.Equal(1000,employees.SalaryBudget("Employee2"));
+ 
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            var lines = File.ReadAllLines("../../../test2.txt");
+            Employees employees = new Employees(lines);
+            Assert.Equal(3800,employees.SalaryBudget("Employee1"));
+            Assert.Equal(1800,employees.SalaryBudget("Employee2"));
+            Assert.Equal(500,employees.SalaryBudget("Employee3"));
+ 
+        }
+        
+        //test invalid salary value for employee 6
+        [Fact]
+        public void Test3()
+        {
+            var lines = File.ReadAllLines("../../../test3.txt");
+            Employees employees = new Employees(lines);
+            Assert.Equal(3300,employees.SalaryBudget("Employee1"));
+            Assert.Equal(1300,employees.SalaryBudget("Employee2"));
+            Assert.Equal(500,employees.SalaryBudget("Employee3"));
+            Assert.Equal(0,employees.SalaryBudget("Employee6"));
  
         }
     }
